5a5bca036bc7038b3e3953d709acf47c8013967f
Absurdius <e94doclrd@hotmail.com>
2017-10-03 20:29:08 +0200

    Skeleton for queue done

--- Assets/Queuemanager.cs
+++ Assets/Queuemanager.cs
@@ -4,7 +4,40 @@ using UnityEngine;
 
 public class Queuemanager : MonoBehaviour {
 
+	// classic FIFO queue
 	// holds a queue 
-
+	private GameObject[] queue; 
 	
+	public void qelement(string name)
+	{
+		//create new text 
+		private GameObject nameObject;
+		nameObject = new Text(name); 
+		
+		//add to queue
+		queue.add(nameObject);
+	}
+	
+	//called when someone wants to exit
+	public void moveup () 
+	{
+		//delete first element in queue
+		
+		//animate upward movement of the rest
+		//DO NOT SWAP WITHIN THE ARRAY	
+		
+	}
+	
+	private GameObject findFirst()
+	{
+		int indx = 0; 
+		while (queue[indx] == null){indx++;}
+		return queue[indx]; 
+	}
+	
+	//called once per frame use this to animate
+	void Update()
+	{
+		//trs.Translate(0 ,0, 0); Sample translation
+	}
 }
