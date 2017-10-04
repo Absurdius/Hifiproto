56e7a30df74e6b8a30112a0ce854fab85c3b3942
Absurdius <e94doclrd@hotmail.com>
2017-10-04 14:17:07 +0200

    queue works but needs alignment

--- Assets/Queuemanager.cs
+++ Assets/Queuemanager.cs
@@ -1,43 +1,71 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Queuemanager : MonoBehaviour {
 
-	// classic FIFO queue
-	// holds a queue 
-	private GameObject[] queue; 
+	private Transform trs;
+	private Queue<GameObject> generalqueue;
+	public GameObject bubble; 
 	
-	public void qelement(string name)
-	{
-		//create new text 
-		private GameObject nameObject;
-		nameObject = new Text(name); 
-		
-		//add to queue
-		queue.add(nameObject);
+	void Start(){
+	trs = GetComponent<Transform>();
+	generalqueue = new Queue<GameObject>(); 
+	}
+	
+	private float goal;
+	
+	//called when user wants ehe word
+	public void qelement(String name)
+	{		
+		GameObject obj = Instantiate(bubble);
+		obj.transform.SetParent(trs);
+		generalqueue.Enqueue(obj); 
+		//writeResults();
 	}
 	
 	//called when someone wants to exit
 	public void moveup () 
 	{
-		//delete first element in queue
-		
-		//animate upward movement of the rest
-		//DO NOT SWAP WITHIN THE ARRAY	
-		
+		if(generalqueue.Count != 0)
+		{
+		GameObject first = generalqueue.Dequeue();
+		Destroy(first);
+		writeResults();
+		}
 	}
 	
-	private GameObject findFirst()
+	private void writeResults()
 	{
-		int indx = 0; 
-		while (queue[indx] == null){indx++;}
-		return queue[indx]; 
+		int indx = 0;
+		foreach (GameObject name in generalqueue)
+		{
+			goal = 100 - 50*indx; 
+			Transform trans = name.GetComponent<Transform>();
+			trans.Translate(0, goal, 0);
+			indx++;
+		}
 	}
 	
-	//called once per frame use this to animate
+	
+	
+	/*
+	called once per frame use this to animate
 	void Update()
 	{
-		//trs.Translate(0 ,0, 0); Sample translation
+		if(animate)
+		{ 
+			foreach (GameObject name in generalqueue)
+			{
+				goal = -100 - 20*indx; 
+				Transform trans = name.GetComponent<Transform>();
+				if(trs.position.y < goal){}
+				
+			}
+		}
 	}
+	*/
+	
 }
